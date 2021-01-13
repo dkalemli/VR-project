7c336dfb6acc654c5aa0fc783b357ed92d5fdd08
Max <s1121610@student.hsleiden.nl>
2021-01-13 14:00:29 +0100

    alle pushes verplaatst uit branch versie_0.1

---
+++ BuitenMAIN/buiten_js/main.js
@@ -0,0 +1,65 @@
+console.log("running");
+const places = document.getElementsByClassName('js--place');
+const camera = document.getElementById('js--camera');
+
+let pickups = document.getElementsByClassName('js--pickup');
+let hold = null;
+
+let placeholders = document.getElementsByClassName('js--placeholder');
+let scene = document.getElementById('js--scene');
+
+let afstand = Math.sqrt((this.getAttribute('position').x - camera.x)*2 + (this.getAttribute('position').z - camera.z)*2)
+
+function addListeners(){
+    for (let i = 0; i < pickups.length; i++){
+        pickups[i].addEventListener('click', function(evt){
+            if (hold == null) {
+                camera.innerHTML += '<a-box id="js--hold" class="js--pickup js--interact" color="green" position="1 -1 -1"></a-box>';
+                hold = "box";
+                this.remove();
+            }
+        });
+    }
+}
+
+addListeners();
+
+// for (let i = 0; i < pickups.length; i++){
+//     pickups[i].addEventListener('click', function(evt){
+//         if (hold == null) {
+//             camera.innerHTML += '<a-box id="js--hold" class="js--pickup js--interact" color="green" position="1 -1 -1"></a-box>';
+//             hold = "box";
+//             this.remove();
+//         }
+//     });
+// }
+
+let speedtime = 2000;
+
+    for (let i = 0; i < places.length; i++){
+    places[i].addEventListener('click', function(evt){
+        // camera.setAttribute('position', this.getAttribute('position').x + " 1.6 " + this.getAttribute('position').z);
+        let att = document.createAttribute("animation");
+        att.value = "property: position; easing: linear; dur: speedtime; to: " + this.getAttribute('position').x + " 1.6 " + this.getAttribute('position').z;
+        camera.setAt
+        tribute('animation', att.value);
+    });
+
+}
+
+
+for (let i = 0; i < placeholders.length; i++){
+    placeholders[i].addEventListener('click', function(evt){
+        if (hold == "box") {
+            let box = document.createElement('a-box');
+            box.setAttribute("class", "js--pickup js--interact");
+            box.setAttribute("color", "green");
+            box.setAttribute("position", {x: this.getAttribute('position').x, y:"0.5", z:this.getAttribute('position').z});
+            // let speed = this.getAttribute('position').x * this.getAttribute('position').x * this.getAttribute('position').z * this.getAttribute('position').z;
+            scene.appendChild(box);
+            document.getElementById("js--hold").remove();
+            addListeners();
+            hold = null;
+        }
+    });
+}
---
+++ BuitenMAIN/buiten_js/walk.js
@@ -0,0 +1,77 @@
+window.onload = () =>{
+    const places = document.getElementsByClassName('js--place');
+    const camera = document.getElementById('js--camera');
+  
+    let pickups = document.getElementsByClassName('js--pickup');
+    let hold = null;
+  
+    const placeholders = document.getElementsByClassName('js--placeholder');
+    let scene = document.getElementById('js--scene');
+  
+    function addListeners() {
+      for (let i = 0; i < pickups.length; i++) {
+        pickups[i].addEventListener('click', function(evt){
+          if (hold == null) {
+            camera.innerHTML += '<a-box id="js--hold" class="js--pickup js--interact" color="green" position="1 -1 -1"></a-box>';
+            hold = "box";
+            this.remove();
+          }
+        });
+      }
+    }
+  
+    addListeners();
+  
+    for (let i = 0; i < placeholders.length; i++) {
+      placeholders[i].addEventListener('click', function(evt){
+        if (hold == "box"){
+          let box = document.createElement('a-box');
+          box.setAttribute("class", "js--pickup js--interact");
+          box.setAttribute("color", "green");
+          box.setAttribute("position", {x: this.getAttribute('position').x, y:"6", z: this.getAttribute('position').z});
+          scene.appendChild(box);
+          document.getElementById("js--hold").remove();
+          addListeners();
+          hold = null;
+        }
+      });
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    // for (let i = 0; i < places.length; i++) {
+    //   places[i].addEventListener('click', function(evt){
+    //     let att = document.createAttribute("animation");
+    //     att.value = "property: position; easing: linear; dur: 2000; to: " + this.getAttribute('position').x + " 1.6 " + this.getAttribute('position').z;
+    //     camera.setAttribute('animation', att.value);
+    //   });
+    // }
+  
+  
+    for (let i = 0; i < places.length; i++) {
+      places[i].addEventListener('click', function(evt){
+        let att = document.createAttribute("animation");
+  
+        var afstand = Math.sqrt(Math.pow((this.getAttribute('position').x - camera.getAttribute("position").x), 2) + Math.pow((this.getAttribute('position').z - camera.getAttribute("position").z), 2));
+        console.log(afstand);
+  
+        const snelheid = 250 * afstand;
+        let speed = snelheid;
+        if(afstand < 170){
+          att.value = "property: position; dur: " + speed + "; easing: linear; to: " + this.getAttribute('position').x + " 6 " + this.getAttribute('position').z;
+          camera.setAttribute('animation', att.value);
+        }
+  
+  
+        // console.log(this.getAttribute('position').x, this.getAttribute('position').z, camera.getAttribute("position").x, camera.getAttribute("position").z);
+      });
+    }
+  };
+  
+  
