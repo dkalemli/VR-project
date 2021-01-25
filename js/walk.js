ca8398480963b6093a886ca61850d4b0cd6bf2c2
Max Osewoudt <s1121610@student.hsleiden.nl>
2021-01-25 11:04:20 +0100

    tegels toegevoegd

--- js/walk.js
+++ js/walk.js
@@ -29,6 +29,7 @@ window.onload = () =>{
           box.setAttribute("class", "js--pickup js--interact");
           box.setAttribute("color", "green");
           box.setAttribute("position", {x: this.getAttribute('position').x, y:"6", z: this.getAttribute('position').z});
+
           scene.appendChild(box);
           document.getElementById("js--hold").remove();
           addListeners();
@@ -52,7 +53,7 @@ window.onload = () =>{
         var afstand = Math.sqrt(Math.pow((this.getAttribute('position').x - camera.getAttribute("position").x), 2) + Math.pow((this.getAttribute('position').z - camera.getAttribute("position").z), 2));
         console.log(afstand);
   
-        const snelheid = 250 * afstand;
+        const snelheid = 300 * afstand;
         let speed = snelheid;
         
         if(afstand < 250){
@@ -64,6 +65,44 @@ window.onload = () =>{
         // console.log(this.getAttribute('position').x, this.getAttribute('position').z, camera.getAttribute("position").x, camera.getAttribute("position").z);
       });
     }
-  };
+  
+
+  //let stoplicht = document.getElementById("js--stoplicht");
+  let rood = document.getElementById("js--stoplicht_rood");
+  let groen = document.getElementById("js--stoplicht_groen");
+  console.log(rood);
+  //let scene = document.getElementById('js--scene');
+
+
+  function opGroen(){
+      setTimeout(function(){
+          rood.setAttribute('color', "black");
+          groen.setAttribute('color', "green");  
+          //Robot verschijnt aan overkant
+          let cylinder = document.getElementById("Cylinder");
+          //cylinder.setAttribute("class", "js--place js--interact");
+          //cylinder.setAttribute("color", "red");
+          //cylinder.setAttribute("radius", "1.2");
+          //cylinder.setAttribute("height", "1");
+          cylinder.setAttribute("position", {x: "-2", y: "1", z: "315"});
+          //scene.appendChild(cylinder);
+          //scene.innerHTML += '<a-cylinder class="js--place js--interact" color="red" height="1" radius="1.2" position="-2 1 293"></a-cylinder>';
+          camera.innerHTML += '<a-obj-model scr="./models/Robot.obj" mtl="./models/Robot.mtl" position="2 -1 -2"></a-box>';
+
+
+    }, 30000);
+  }
+
+opGroen();
+};
+
+
+function zebrapad(){
+  remmen = document.getElementsByClassName("js--remmen");
+  remmen.removeAttribute("animation__rijden");
+  setTimeout(function(){
+    remmen.setAttribute("animation__rijden", "dur: 15000; easing: linear; from:10 1 -320; to: 10 1 550; loop:-1; property:position");
+  }, 50000);
+}
   
   
